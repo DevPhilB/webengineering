d8518be171d54f2655212ad121d86dd3da8d122d
Sam Fri <samuel444@web.de>
2019-11-05 15:44:48 +0100

    First settings for GET and POST Handler

--- aufgabe1/material-webserver/src/main/java/de/uniulm/in/ki/webeng/serverscaffold/httphandlers/GETHandler.java
+++ aufgabe1/material-webserver/src/main/java/de/uniulm/in/ki/webeng/serverscaffold/httphandlers/GETHandler.java
@@ -1,5 +1,6 @@
 package de.uniulm.in.ki.webeng.serverscaffold.httphandlers;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 import de.uniulm.in.ki.webeng.serverscaffold.MIMEHandler;
@@ -11,27 +12,36 @@ import de.uniulm.in.ki.webeng.serverscaffold.model.Response;
  * Handles GET requests Created by Markus Brenner on 07.09.2016.
  */
 public class GETHandler {
-    /**
-     * Handles a request
-     *
-     * @param request
-     *            The request issued by a client
-     * @param response
-     *            An empty response object, which is to be filled with the
-     *            correct reply
-     */
-    public static void handle(Request request, Response response) {
-        // TODO: complete
-        String resource = request.resource.substring(1);
-        resource = (resource.length() == 0) ? "index.html" : resource;
-        Path resourcePath = ServerConfiguration.webRoot.resolve(resource);
-        if (!resourcePath.normalize()
-                .startsWith(ServerConfiguration.webRoot.normalize())) {
-            // TODO; set response code
-        } else {
-            // TODO: set body, content length header and response code
-            response.addHeader("Content-Type",
-                    MIMEHandler.getMimeType(resourcePath));
-        }
-    }
+	/**
+	 * Handles a request
+	 *
+	 * @param request  The request issued by a client
+	 * @param response An empty response object, which is to be filled with the
+	 *                 correct reply
+	 */
+	public static void handle(Request request, Response response) {
+		// TODO: complete
+		String resource = request.resource.substring(1);
+		resource = (resource.length() == 0) ? "index.html" : resource;
+		Path resourcePath = ServerConfiguration.webRoot.resolve(resource);
+		System.out.println("RESOURCE '" + resource + "' PATH '" + resourcePath + "'");
+		if (!resourcePath.normalize().startsWith(ServerConfiguration.webRoot.normalize())) {
+			// TODO; set response code
+			response.setResponseCode(401, "Unauthorized");
+			response.addHeader("Connection", "close");
+		} else {
+			// TODO: set body, content length header and response code
+			response.setResponseCode(200, "OK");
+			response.addHeader("Content-Type", MIMEHandler.getMimeType(resourcePath));
+			response.addHeader("Connection", "Keep-Alive");
+			try {
+				response.setBody(resourcePath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				response.setResponseCode(400, "Not found");
+				response.addHeader("Connection", "close");
+				e.printStackTrace();
+			}
+		}
+	}
 }
