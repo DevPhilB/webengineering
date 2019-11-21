7765e672110fcb6499ee684fcb9b93561ce17d21
Philipp Backes <philippbackes95@gmail.com>
2019-11-21 15:40:56 +0100

    Prepare exercise 6

--- aufgabe2/JavaWebserver-06/src/main/java/de/uniulm/in/ki/webeng/serverscaffold/ResponseValidator.java
+++ aufgabe2/JavaWebserver-06/src/main/java/de/uniulm/in/ki/webeng/serverscaffold/ResponseValidator.java
@@ -1,21 +1,28 @@
 package de.uniulm.in.ki.webeng.serverscaffold;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
-
-import org.w3c.dom.Document;
-
-import de.uniulm.in.ki.webeng.serverscaffold.model.Response;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import de.uniulm.in.ki.webeng.serverscaffold.model.Response;
 
 public class ResponseValidator {
     /**
@@ -45,6 +52,19 @@ public class ResponseValidator {
     }
 
     /**
+     * Deletes the cache file if it exists
+     */
+    public static void clearCache() {
+        if (Files.exists(ServerConfiguration.cachePath)) {
+            try {
+                Files.delete(ServerConfiguration.cachePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
      * Stores a response to the local cache
      *
      * @param remoteResponse
@@ -62,16 +82,6 @@ public class ResponseValidator {
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void clearCache() {
-        if (Files.exists(ServerConfiguration.cachePath)) {
-            try {
-                Files.delete(ServerConfiguration.cachePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
@@ -106,18 +116,20 @@ public class ResponseValidator {
     }
 
     private static Document extractXML(Response remoteResponse) {
-        DocumentBuilder documentBuilder = null;
-        InputStream bodyStream = null;
-        Document document = null;
+        String xml = null;
         try {
-            documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            bodyStream = new ByteArrayInputStream(remoteResponse.getBody());
-            document = documentBuilder.parse(bodyStream);
-            bodyStream.close();
+            xml = new String(remoteResponse.getBody(), "UTF-8");
+            DocumentBuilderFactory factory = DocumentBuilderFactory
+                    .newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            // suppress err messages to System.err.
+            builder.setErrorHandler(null);
+            InputSource is = new InputSource(new StringReader(xml));
+            Document doc = builder.parse(is);
+            return doc;
         } catch (Exception e) {
-            e.printStackTrace();
+            return null;
         }
-        return document;
     }
 
     /**
@@ -128,36 +140,25 @@ public class ResponseValidator {
      * @return True, if the provided XML is valid
      */
     public static boolean isValidXML(Response remoteResponse) {
-        SchemaFactory schemaFactory = null;
-        Source schemaSource = null;
-        Schema schema = null;
-        Validator validator = null;
-        Document document = extractXML(remoteResponse);
-        if (document == null)
-            return false;
-
-        File schemaFile = new File("PriceSchema.xsd");
         try {
-            schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            // Load the XML schema
-            schemaSource = new StreamSource(schemaFile);
-            schema = schemaFactory.newSchema(schemaSource);
+            Document doc = extractXML(remoteResponse);
+            if (doc == null)
+                return false;
 
-            // Create a Validator instance, which can be used to validate an instance document
-            validator = schema.newValidator();
-        } catch (Exception e) {
+            // create a validator using the schema
+            File schemaFile = new File("schema.xsd");
+            SchemaFactory schemaFactory = SchemaFactory
+                    .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(schemaFile);
+            Validator validator = schema.newValidator();
+            validator.validate(new DOMSource(doc));
+            return true;
+        } catch (SAXException | IOException e) {
+            // on any error, assume the XML is broken and return false
             e.printStackTrace();
-        }
-
-        // Validate the DOM tree
-        try {
-            validator.validate(new DOMSource(document));
-        } catch (Exception e) {
-            // XML is invalid!
             return false;
         }
 
-        return true;
     }
 
     /**
@@ -169,6 +170,17 @@ public class ResponseValidator {
      * @return A transformed response
      */
     public static Response transformResponse(Response remoteResponse) {
+        Document doc = extractXML(remoteResponse);
+
+        // load the XSLT
+        // TODO implement
+
+        // setup and perform transformation
+        // TODO implement
+
+        // adapt remoteResponse body and content-type header
+        // TODO implement
+
         return remoteResponse;
     }
 }
