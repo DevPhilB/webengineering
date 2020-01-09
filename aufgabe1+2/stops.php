e8261e501a322751e0be1046390c8dcd2cd1b888
Philipp Backes <philippbackes95@gmail.com>
2020-01-09 15:08:49 +0100

    Prepare exercise 11

--- aufgabe1+2/stops.php
+++ aufgabe1+2/stops.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>WebEng 2018 - Haltestellen</title>
+        <title>WebEng 2019 - Haltestellen</title>
         <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="style.css">
     </head>
@@ -12,8 +12,10 @@
       <header>
           <h1>Web Engineering Routenplaner</h1>
           <nav>
-              <a href="index.html">Home</a>
-              <a class="active" href="stops.html">Stops</a>
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a class="active" href="stops.html">Stops</a></li>
+            </ul>
           </nav>
       </header>
       <main class="stop-content">
@@ -41,7 +43,7 @@
             </div>
         </main>
         <footer>
-                Copyright WebEng 2018, Maybe rights are reserved
+                Copyright WebEng 2019, Maybe rights are reserved
         </footer>
     </body>
 </html>
